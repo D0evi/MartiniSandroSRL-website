c1edfc14f432b38007c738b171aa62466e5bea61
Davide <Davide4.martini@mail.polimi.it>
2025-10-05 18:19:32 +0200

    c

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
 // Endpoint Web App di Google Apps Script per il form contatti
 // Istruzioni più sotto su come ottenerlo; lasciare il placeholder finché non è pronto
-export const FORM_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbx28rIqw40ukNClfqA8TcLbMMmdOfH1bEZkhOH3s_o/dev';
+export const FORM_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbx28rIqw40ukNClfqA8TcLbMMmdOfH1bEZkhOH3s_o/exec';
